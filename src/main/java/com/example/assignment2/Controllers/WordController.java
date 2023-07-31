8ab1a242a933a80f056ad7c3405d8082b43477d8
Anton Korchynskyi <200524341@student.georgianc.on.ca>
2023-07-31 19:17:23 -0400

    Added css file and updated the word view

--- src/main/java/com/example/assignment2/Controllers/WordController.java
+++ src/main/java/com/example/assignment2/Controllers/WordController.java
@@ -1,11 +1,13 @@
 package com.example.assignment2.Controllers;
 
 import com.example.assignment2.Models.Definitions;
+import com.example.assignment2.Models.Word;
 import com.example.assignment2.Utilities.ApiUtility;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
@@ -13,6 +15,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class WordController implements Initializable {
+
+    @FXML
+    private Label definitionsLabel;
+
+    @FXML
+    private Label mainLabel;
 
     @FXML
     private ListView<Definitions> listView;
@@ -33,11 +41,21 @@ public class WordController implements Initializable {
 
     @FXML
     void search_onClick(ActionEvent event) {
+        listView.getItems().clear();
+        String searchText = textField.getText();
 
+        Word newWord = ApiUtility.getWordFromApi(searchText);
+
+        if(newWord.getDefinitions() == null){
+            mainLabel.setText("No Word Found!");
+        } else{
+            listView.getItems().addAll(newWord.getDefinitions());
+            mainLabel.setText("Choose Your Word!");
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        listView.getItems().addAll(ApiUtility.getWordFromApi("fate").getDefinitions());
+//        listView.getItems().addAll(ApiUtility.getWordFromApi("fate").getDefinitions());
     }
 }
